3356a4fb60f4f40f1b8174ae500ad8b28b1340b3
TeaBagYan <92146993+TeaBagYan@users.noreply.github.com>
2021-11-19 13:01:28 +0600

    Add files via upload

---
+++ дз 19.11/js/main.js
@@ -0,0 +1,24 @@
+function but1(){
+let name = prompt("Введите свое имя")
+document.getElementById('name').textContent="Ваше имя:"+ name
+}
+function but2(){
+let sname = prompt("Введите свою фамилию")
+document.getElementById('sname').textContent="Ваша фамилия:"+ sname
+}
+function but3(){
+let age = prompt("Введите свой возраст")
+document.getElementById('age').textContent="Ваш возраст:"+ age
+}
+function but4(){
+let spec = prompt("Введите свою специальность")
+document.getElementById('spec').textContent="Ваша специальность:"+ spec
+}
+function but5(){
+let bday = prompt("Введите свою дату рождения")
+document.getElementById('bday').textContent="Ваша дата рождения:"+ bday
+}
+function but6(){
+let osebe = prompt("Введите немного информации о себе")
+document.getElementById('osebe').textContent="Ваша информация:"+ osebe
+}
